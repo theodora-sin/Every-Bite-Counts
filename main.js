fe0fdf883d045eb8ab62d42b56a737479065edc5
theodora-sin <theodrasin@gmail.com>
2026-07-24 11:59:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 /* Global Variables */
-let screen;
+let screen=0;
 
-//background
-let bg0,chat,mini1, street, foodie, foodie1;
 
 //Screen0
 let startButton;
+let startBg
 
 //Screen 1
 let charImg1, charImg2;
@@ -17,37 +16,33 @@ let prefaceButton;
 let prefaceShown =false;
 
 //Screen3, screen 4
-let choosebutton3, choosebutton4;
+//let choosebutton3, choosebutton4;
 
 //Screen 5, define within the program
 
 //Screen 6
-let choosebutton5;
+//let choosebutton5;
 
 //screen 7
-let continueButton1,continueButton2;
-let rottenFoodImage={};
-let player,walls, obstacles;
-let mazeStarted=false;
-let mazeGameover=false;
-let mazeWon=false;
-let mazeCollected=0;
-let showEndZoneHint=false;
+//let continueButton1,continueButton2;
+//let rottenFoodImage={};
+//let player,walls, obstacles;
+//let mazeStarted=false;
+//let mazeGameover=false;
+//let mazeWon=false;
+//let mazeCollected=0;
+//let showEndZoneHint=false;
 
 /* PRELOAD LOADS FILES */
 function preload() {
     charImg1=loadImage("assets/max.jpg");
     charImg2=loadImage("assets/maya.jpg");
-    bg0=loadImage("assets/cafe.jpg");
-    chat=loadImage("assets/chat.jpg");
+    startBg=loadImage("assets/start.jpg")
     mini1=loadImage("assets/mini1.jpg");
-    street=loadImage("assets/street.jpg");
-    foodie=loadImage("assets/foodie.jpg");
-    foodie1=loadImage("assets/foodie_colour.jpg");
     rottenFoodImage={
         apple=loadImage("assets/apple.png"),
         avocado=loadImage("assets/avocado.png"),
-        canfish=oadImage("assets/canfish.png"),
+        canfish=loadImage("assets/canfish.png"),
         fishcan=loadImage("assets/fishcan.png"),
         flour=loadImage("assets/flour.png"),
     }
@@ -79,27 +74,5 @@ function draw(){
         screen1();
     }else if (screen==2){
         screen2();
-    }else if(screen==3){
-        chapter1()
-    }else if(screen==4){
-        dialogue1()
-    }else if (screen==5){
-        if(!miniGameStarted){
-            introScreen();
-        }else if (gameOver){
-            drawEndScreen();
-        }else{
-            catchingGame();
-        }
-    }else if (screen==6){
-        chapter2();
-    }else if(screen==7){
-        if(!mazeStarted){
-            mazeGame();
-        }else if(mazeGameOver){
-            drawMazeEndScreen();
-        }else{
-            runMazeGame();
-        }
     }
 }
